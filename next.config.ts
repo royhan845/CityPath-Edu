1c6c2b97efccd0160b65b35ebc927b5e94d464fa
Royhan <royhanfirdaus12345@gmail.com>
2026-05-24 20:27:47 +0700

    fix: enforce webpack build for next-pwa compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+
+// @ts-expect-error: package next-pwa belum memiliki tipe bawaan untuk TypeScript
 import withPWAInit from "next-pwa";
 
 // Konfigurasi PWA
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development", 
   register: true,
   skipWaiting: true,
 });
@@ -13,4 +15,5 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// Bungkus nextConfig dengan PWA
 export default withPWA(nextConfig);
